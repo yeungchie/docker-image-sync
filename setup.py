e822bb36591243f30440a5f4fa99e1ab2d4ba011
YEUNGCHIE <yeungchie@yeungchie.top>
2024-07-29 21:34:57 +0800

    upload requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+here = Path(__file__).resolve().parent
+
+with open(here / 'README.md', encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Docker Image Synchronizer'
+
+setup(
+    name='docker-image-sync',
+    version=VERSION,
+    author='YEUNGCHIE',
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yeungchie/docker-image-sync',
+    packages=find_packages(),
+    install_requires=[
+        'PyYAML>=6.0.1',
+        'docker>=5.0.3',
+        'rich>=12.6.0',
+    ],
+    keywords=['python', 'docker', 'image', 'sync', 'linux'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+    ],
+)
