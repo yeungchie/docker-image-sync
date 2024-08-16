7a8dfc5d73aefcaaa94dc0ea6aebce57aff4ab62
YEUNGCHIE <yeungchie@yeungchie.top>
2024-08-17 04:58:00 +0800

    Add __main__

--- docker_image_sync/purge.py
+++ docker_image_sync/purge.py
@@ -39,7 +39,7 @@ def getContainers(image_id: Union[str, Image]) -> List[Container]:
     return containers
 
 
-def purge_by_name(name: str) -> None:
+def purge_by_name(name: str, logHandle=print) -> None:
     try:
         image = CLIENT.images.get(name)
     except ImageNotFound:
@@ -48,21 +48,83 @@ def purge_by_name(name: str) -> None:
     containers = getContainers(image)
     if len(containers) > 0:
         container_names = '[+' + ', '.join(str(c.name) for c in containers) + ']'
-        print(f'Image "{name}" is being used for container: {container_names}')
+        logHandle(f'Image "{name}" is being used for container: {container_names}')
         return
 
     CLIENT.images.remove(name)
-    print(f'Image "{image.tags}" is removed')
+    logHandle(f'Image "{image.tags}" is removed')
 
 
-def purge_none() -> None:
-    for image in CLIENT.images.list():
+def purge_none(logHandle=print, progress=None) -> None:
+    image_list = CLIENT.images.list()
+    task = None
+    if progress is not None:
+        task = progress.add_task("[red]Invalid Image Purge", total=len(image_list))
+
+    for image in image_list:
         if not image.tags:
             containers = getContainers(image)
             if len(containers) > 0:
                 names = '[+' + ', '.join(str(c.name) for c in containers) + ']'
-                print(f'Image "{image.short_id}" is being used for container: {names}')
+                logHandle(
+                    f'Image "{image.short_id}" is being used for container: {names}'
+                )
+                if progress is not None and task is not None:
+                    progress.update(task, advance=1)
                 continue
 
             CLIENT.images.remove(image.id, force=True)
-            print(f'Image "{image.short_id}" is removed')
+            logHandle(f'Image "{image.short_id}" is removed')
+            if progress is not None and task is not None:
+                progress.update(task, advance=1)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parse = argparse.ArgumentParser()
+    parse.add_argument('config', help='yaml file')
+    parse.add_argument('--try-run', action='store_true', help='try run')
+    args = parse.parse_args()
+
+    if not args.config:
+        print('config file is required')
+        exit(1)
+
+    import yaml
+    from rich.console import Console, NewLine
+    from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
+    from rich.rule import Rule
+
+    with open(args.config, 'r') as f:
+        config = yaml.full_load(f)
+
+    registry = config['registry']
+    namespace = config.get('namespace', None)
+    images = []
+
+    if namespace is None:
+        domain = registry
+    else:
+        domain = f'{registry}/{namespace}'
+
+    for image in config['images']:
+        images.append(f'{domain}/{image}')
+
+    with Progress(
+        SpinnerColumn(),
+        *Progress.get_default_columns(),
+        TimeElapsedColumn(),
+        console=Console(log_time_format='[%F %T]'),
+        transient=False,
+    ) as progress:
+        progress.log(Rule('[red]Invalid Image Purge'), NewLine(1))
+        purge_none(logHandle=progress.log, progress=progress)
+        temp_purge_task = progress.add_task(
+            '[yellow]Temp Image Purge', total=len(images)
+        )
+        progress.log(NewLine(1))
+        progress.log(Rule('[yellow]Temp Image Purge'), NewLine(1))
+        for image in images:
+            purge_by_name(image, logHandle=progress.log)
+            progress.update(temp_purge_task, advance=1)
